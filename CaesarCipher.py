9d181f5c75728d4707003ffcb5998a17370930ec
slcc2c <slcc2c@mail.umkc.edu>
2017-01-27 03:01:52 -0600

    Added first attempt at decrypt

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -12,4 +12,7 @@ def encrypt(message, key):
             output += chr((ascii_letter)%90+(0 if ascii_letter>64 else 65))
     print(output)
 
+def decrypt(message, key):
+    print(encrypt(message,-1*int(key)))
+
 encrypt(input("what message would you like to encrypt?"), int(input("what is your key?")))
