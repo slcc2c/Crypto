264f54f84bd5c96393243282c4e311881606ba3d
slcc2c <slcc2c@mail.umkc.edu>
2017-01-27 16:11:48 -0600

    Finished decode

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -11,5 +11,26 @@ def encrypt(message, key):
         if (ascii_letter in range(65, 90)):
             output += chr((ascii_letter)%90+(0 if ascii_letter>64 else 65))
     print(output)
+    return output
+def decrypt(message, key):
+    output = ''
+    for letter in message:
 
-encrypt(input("what message would you like to encrypt?"), int(input("what is your key?")))
+        ascii_letter = ord(letter)
+
+        #lower case
+        if(ascii_letter in range(97,122)):
+            ascii_letter-=key
+            if(ascii_letter<97):
+                ascii_letter+=26
+            output += chr((ascii_letter)%122+(0 if ascii_letter>96 else 97))
+        #upper case
+        if (ascii_letter in range(65, 90)):
+            ascii_letter -= key
+            if (ascii_letter < 65):
+                ascii_letter += 26
+            output += chr((ascii_letter)%90+(0 if ascii_letter>64 else 65))
+    print(output)
+    return output
+
+decrypt(encrypt(input("what message would you like to encrypt?"), int(input("what is your key?"))),int(input("what is your key?")))
