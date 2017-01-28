df8d1817e06fab306f5a8e51baf374609c4243ed
slcc2c <slcc2c@mail.umkc.edu>
2017-01-27 18:49:57 -0600

    Implemented encrypt/decrypt for Vigenere cipher

---
+++ VigenereCipher.py
@@ -0,0 +1,24 @@
+def encrypt(message, key):
+    counter = 0
+    output=''
+
+    for letter in message:
+        ascii_letter = ((ord(letter)-97+ord(key[counter])-97) %26)+98
+        counter = (counter+1)%len(key)
+        output+=chr(ascii_letter)
+    return output
+
+def decrypt(message, key):
+    counter = 0
+    output=''
+
+    for letter in message:
+        ascii_letter = ((ord(letter)-ord(key[counter])))
+        counter = (counter+1)%len(key)
+        if ascii_letter < 0:
+            output+=chr(ascii_letter+122)
+        else:
+            output+=chr(ascii_letter+96)
+    return output
+print(encrypt("test","as"))
+print(decrypt("uxtm","as"))
